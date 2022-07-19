4df861ae6019f10fe7143f196b1e6d35e1167348
CurryBetweenRice <williamwijaya25@gmail.com>
2022-07-19 09:19:15 +0700

    Build add

---
+++ build/precache-manifest.c8eac17295b6740c3dc2f372c7ae121e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7f21fe83ddea8586e7de1aa3696733",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "b55f1484c7083cd2f12b",
+    "url": "/calculator/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "b55f1484c7083cd2f12b",
+    "url": "/calculator/static/js/main.8d7bd8f1.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
